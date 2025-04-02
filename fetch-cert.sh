8b81b801a87feb1656c477dda48360898fd9d90d
iamantil <iantil101@gmail.com>
2025-04-02 06:53:34 +0000

    Adding files to fetch the certificate inside the container

---
+++ fetch-cert.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Pull the docker image from the container registry
+docker pull myteqhub/hvclient:latest
+
+echo "Please wait pulling the hvclient nimage"
+sleep 30
+
+# Start the container
+docker run -d --name hvclient --volume /home/ubuntu/.hvclient:/root/.hvclient myteqhub/hvclient:latest tail -f /dev/null
+
+sleep 5
+
+# List the name of the running container
+CONTAINER_NAME=$(docker ps --format "{{.Names}}" | head -n 1)
+
+# Print the stored container name
+echo "Stored Container Name: $CONTAINER_NAME"
+
+#Copy the script inside the container
+docker cp /home/ubuntu/hvclient.sh $CONTAINER_NAME:/root/.hvclient/hvclient.sh
+
+#Make the script executable
+docker exec -it $CONTAINER_NAME chmod +x /root/.hvclient/hvclient.sh
+
+
+
+#Execute the script into the container
+docker exec -d $CONTAINER_NAME /root/.hvclient/hvclient.sh
---
+++ hvclient.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+openssl genrsa 2048 > /root/.hvclient/test.key
+/hvclient -trustchain > /root/.hvclient/ca_chain.pem
+/hvclient -privatekey /root/.hvclient/test.key -commonname pki.atlasqa.co.uk -csrout > /root/.hvclient/csr.pem
+/hvclient -commonname pki.atlasqa.co.uk -dnsnames=pki.atlasqa.co.uk -sighash=SHA-256 -csr /root/.hvclient/csr.pem > /root/.hvclient/tls.crt
+tail -f /dev/null
