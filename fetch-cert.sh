e9e13d535e92e15275a3f1c4284e37642d4cdb77
iamantil <iantil101@gmail.com>
2025-04-03 14:12:18 +0000

    Copying the cert and key into the container

--- fetch-cert.sh
+++ fetch-cert.sh
@@ -10,7 +10,7 @@ fi
 # Pull the docker image from the container registry
 docker pull myteqhub/hvclient:latest
 
-echo "Please wait pulling the hvclient nimage"
+echo "Please wait pulling the hvclient image"
 sleep 30
 
 # Start the container
@@ -24,8 +24,14 @@ CONTAINER_NAME=$(docker ps --format "{{.Names}}" | head -n 1)
 # Print the stored container name
 echo "Stored Container Name: $CONTAINER_NAME"
 
-#Copy the script inside the container
-docker cp /home/ubuntu/cicd/SampleRegistratonForm-1/hvclient.sh $CONTAINER_NAME:/root/.hvclient/hvclient.sh
+# Copy the script inside the container
+docker cp /home/ubuntu/hvclient.sh $CONTAINER_NAME:/root/.hvclient/hvclient.sh
+
+# Copy the mTLS cert into the container
+docker copy /home/ubuntu/mTLS.pem $CONTAINER_NAME:/root/.hvclient
+
+# Copy the privatekey into the container
+docker cp /home/ubuntu/privatekey.pem $CONTAINER_NAME:/root/.hvclient
 
 #Make the script executable
 docker exec -it $CONTAINER_NAME chmod +x /root/.hvclient/hvclient.sh
