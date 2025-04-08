06cc4deebbb424123992e476131241c7edf5e9c0
iamantil <iantil101@gmail.com>
2025-04-08 10:28:47 +0000

    removing -sighash=SHA-256 from the hvclient.sh script

--- hvclient.sh
+++ hvclient.sh
@@ -2,5 +2,5 @@
 openssl genrsa 2048 > /root/.hvclient/test.key
 /hvclient -trustchain > /root/.hvclient/ca_chain.pem
 /hvclient -privatekey /root/.hvclient/test.key -commonname pki.atlasqa.co.uk -csrout > /root/.hvclient/csr.pem
-/hvclient -commonname pki.atlasqa.co.uk -dnsnames=pki.atlasqa.co.uk -sighash=SHA-256 -csr /root/.hvclient/csr.pem > /root/.hvclient/tls.crt
+/hvclient -commonname pki.atlasqa.co.uk -dnsnames=pki.atlasqa.co.uk -csr /root/.hvclient/csr.pem > /root/.hvclient/tls.crt
 tail -f /dev/null
