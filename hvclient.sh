6e4047d9e34639c314629fe17d196bb9647ec18e
iamantil <iantil101@gmail.com>
2025-04-21 09:16:52 +0000

    Adding openssl commands to create the PKCS12 inside the container.

--- hvclient.sh
+++ hvclient.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-openssl genrsa 2048 > /root/.hvclient/test.key
+openssl genrsa 2048 > /root/.hvclient/key.pem
 /hvclient -trustchain > /root/.hvclient/ca_chain.pem
-/hvclient -privatekey /root/.hvclient/test.key -commonname pki.atlasqa.co.uk -csrout > /root/.hvclient/csr.pem
-/hvclient -commonname pki.atlasqa.co.uk -dnsnames=pki.atlasqa.co.uk -sighash=SHA-256  -csr /root/.hvclient/csr.pem > /root/.hvclient/tls.crt
-#tail -f /dev/null
+/hvclient -privatekey /root/.hvclient/key.pem -commonname pki.atlasqa.co.uk -csrout > /root/.hvclient/csr.pem
+/hvclient -commonname pki.atlasqa.co.uk -dnsnames=pki.atlasqa.co.uk -sighash=SHA-256  -csr /root/.hvclient/csr.pem > /root/.hvclient/cert.pem
+openssl pkcs12 -export \
+  -in /root/.hvclient/cert.pem \
+  -inkey /root/.hvclient/key.pem \
+  -certfile /root/.hvclient/ca_chain.pem \
+  -out /root/.hvclient/keystore.p12 \
+  -password pass:changeit
