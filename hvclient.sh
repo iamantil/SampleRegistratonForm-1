fb951d1a0e56ad2b35647b689e6a4f1f29b79f00
iamantil <iantil101@gmail.com>
2025-04-25 09:47:20 +0000

    pki.atlasqa.co.uk to devops.atlasqa.co.uk

--- hvclient.sh
+++ hvclient.sh
@@ -3,9 +3,17 @@ echo "Installing JDK"
 apk update
 apk add --no-cache openjdk11
 rm -f /root/.hvclient/keystore.jks
+
+# Generating private key
 openssl genrsa 2048 > /root/.hvclient/key.pem
+
+# Getting the trust chain
 /hvclient -trustchain > /root/.hvclient/ca_chain.pem
+
+# Generate CSR
 /hvclient -privatekey /root/.hvclient/key.pem -commonname devops.atlasqa.co.uk -csrout > /root/.hvclient/csr.pem
+
+
 /hvclient -commonname pki.atlasqa.co.uk -dnsnames=pki.atlasqa.co.uk -sighash=SHA-256  -csr /root/.hvclient/csr.pem > /root/.hvclient/cert.pem
 openssl pkcs12 -export \
   -in /root/.hvclient/cert.pem \
